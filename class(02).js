e85e137bc82660763c7fdb896cc76b546d089b26
Asad Abbas <158063589+asadabbas2004@users.noreply.github.com>
2024-09-11 17:55:23 +0500

    Add files via upload

---
+++ class(02).js
@@ -0,0 +1,107 @@
+// Here is a concise summary of the previous lecture that I've also posted on Google Classroom (GC).
+
+// 1- Alerts
+// Notes
+//     * Alert is a box that pops up to give the user a message.
+//         * Alternatively, it can be written as window.alert but is considered to be highly formal.
+//     * A statement ends in a semicolon.
+
+// Syantx: 
+// alert("hello world");
+   
+// Quiz for practice:
+// http: www.asmarterwaytolearn.com/js/1.html
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------
+// 2 - Variables for Strings
+
+// Notes
+//     * A variable is created when you write var (for variable) followed by the name that you choose to give it.
+
+// Example: 
+// var country = "Pakistan";
+
+// ------------------------------------------------------------------------------------------------------------
+// 3 - Variables for Numbers
+// * Unlike a string, a number is not enclosed in quotes.
+//     * A number enclosed in quotations is a string.
+//     * A variable name cannot begin with a number.
+
+// Example: 
+// var price = 200;
+
+// ------------------------------------------------------------------------------------------------------------
+// 4 - Variables Names Legal and Illegal
+
+// Notes
+//     * Can not enclose variables in quotation marks.
+//     * Can not start with a number or be a number.
+//     * Can not be any of JavaScript's reserved words or keywords. Below is a list of keywords:
+//         * abstract
+//         * alert
+//         * as
+//         * boolean
+//         * break
+//         * byte
+//         * case
+//         * catch
+//         * char
+//         * class
+//         * continue
+//         * const
+//         * debugger
+//         * default
+//         * delete
+//         * do
+//         * double
+//         * else
+//         * enum
+//         * export
+//         * extends
+//         * false
+//         * final
+//         * finally
+//         * float
+//         * for
+//         * function
+//         * goto
+//         * if
+//         * implements
+//         * import
+//         * in
+//         * instanceof
+//         * int
+//         * interface
+//         * is
+//         * long
+//         * namespace
+//         * native
+//         * new
+//         * null
+//         * package
+//         * private
+//         * protected
+//         * public
+//         * return
+//         * short
+//         * static
+//         * super
+//         * switch
+//         * synchronized
+//         * this
+//         * throw
+//         * throws
+//         * transient
+//         * true
+//         * try
+//         * typeof
+//         * use
+//         * var
+//         * void
+//         * volatile
+//         * while
+//         * with
+//     * A variable name can't contain spaces.
+//     * Only letters, numbers, dollar signs, and underscores.
+//     * Variable names are case sensitive. Use camelCase naming convention.
+//     * Balance readability with conciseness.
---
+++ class(03).js
@@ -0,0 +1,90 @@
+// var title = "";
+// // falsy -> empty string
+
+// if (title) {
+//   console.log("TRUE");
+// } else {
+//   console.log("FALSE");
+// }
+
+// var x = 0;
+// // falsy -> 0, undefined, null, NaN
+// // 1, 22
+// if (x) {
+//   console.log("TRUE");
+// } else {
+//   console.log("FALSE");
+// }
+
+// == -> content
+// !=
+// === -> content + type
+// !==
+// var x = 10;
+// var y = "10";
+
+// if (x !== y) {
+//   console.log("Both are equal");
+// } else {
+//   console.log("Both are not equal");
+// }
+
+// var title1 = "Mobile";
+// var title2 = "mobile";
+
+// if (title1.toLocaleLowerCase() != title2.toLocaleLowerCase()) {
+//   console.log("Both are equal");
+// } else {
+//   console.log("Both are not equal");
+// }
+
+// var title = "mobile";
+// console.log(title);
+// console.log(title.length);
+// console.log(title.trim().length);
+
+// var present = false;
+
+// if (present) {
+//   console.log("Present");
+// } else {
+//   console.log("Not Present");
+// }
+
+// ---------------------------------------------------------------------
+// var x = -10;
+// var y = 0;
+// var z = x / y;
+// console.log(z);
+
+// ---------------------------------------------------------------------
+// "use strict";
+
+// var x = 20;
+// console.log(x);
+
+// y = 30;
+// console.log(y);
+
+// ---------------------------------------------------------------------
+// Primitives (basic data type) -> string, number, boolean, undefined, null
+
+// var title = "mobile";
+// console.log(typeof title); // string
+
+// var price = 20000;
+// console.log(typeof price); // number
+
+// var isAvail = true;
+// console.log(typeof isAvail);
+
+// var quantity;
+// console.log(typeof quantity);
+
+// var x = 10;
+// console.log(x)
+
+// var y;
+// console.log(y)
+
+// console.log(z)
